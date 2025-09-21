5fb7cda6fa8b44d8ce1f1794b52177db5d683299
wiertl <jakrop2@wp.pl>
2025-09-21 23:26:27 +0200

    Add files via upload

---
+++ jakubiak_zewnetrzny.js
@@ -0,0 +1,2 @@
+document.write("ostatnia modyfikacja strony".fontcolor("yellow").strike().fontsize(7)+"<br>"); 
+document.write(document.lastModified .fontcolor("yellow").strike().fontsize(7)+"<br>"); 
